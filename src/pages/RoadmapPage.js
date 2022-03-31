0608767fea27b152a2bb7f2b51c89ac080b9db78
Marta Bylewska <work@martabylewska.com>
2022-03-31 14:20:58 +0200

    add checked counter

--- src/pages/RoadmapPage.js
+++ src/pages/RoadmapPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import articleContent from "./article-content";
 import {
@@ -12,18 +12,32 @@ import {
 } from "@mui/material";
 
 function RoadmapPage() {
-  const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
+
+  const [checked, setChecked] = useState(false);
+
+  function checkedChange() {
+    setChecked(
+      document.querySelectorAll("input[type=checkbox]:checked").length
+    );
+  }
+  useEffect(() => {
+    console.log(
+      checked,
+      document.querySelectorAll("input[type=checkbox]").length
+    );
+  }, [checked]);
 
   return (
     <div>
       <Container maxWidth="md" sx={{ width: "50%", my: 4 }}>
         {articleContent.map((article, key) => (
           <Accordion
-            alignItem="center"
+            key={key}
             expanded={expanded === `panel${key}`}
             onChange={handleChange(`panel${key}`)}
           >
@@ -31,14 +45,18 @@ function RoadmapPage() {
               aria-controls="panel1bh-content"
               id="panel1bh-header"
             >
-              <Typography variant="h5" key={key}>
-                {article.title}
-              </Typography>
+              <Typography variant="h5">{article.title}</Typography>
             </AccordionSummary>
 
             <AccordionDetails>
-              {article.content.map((i) => (
-                <FormControlLabel control={<Checkbox />} label={[i]} />
+              {article.content.map((i, key) => (
+                <FormControlLabel
+                  key={key}
+                  control={
+                    <Checkbox checked={checked[i]} onChange={checkedChange} />
+                  }
+                  label={[i]}
+                />
               ))}
             </AccordionDetails>
           </Accordion>
